e9a5063d14fabed135a5a10c7b0f206b615f118a
spaseyhin <evgen.kalin99@gmail.com>
2023-04-13 13:27:21 +0300

    Create room model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" } 
   root "rooms#index"
   devise_scope :user do
-  get '/users/sign_out' => 'devise/sessions#destroy'
-end
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
